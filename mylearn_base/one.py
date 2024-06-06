982108bb9a28c6ec17159f435349a05348c44b80
wangwenxiang <1570679451@qq.com>
2024-06-06 22:28:06 +0800

    python入门

---
+++ mylearn_base/one.py
@@ -0,0 +1,30 @@
+import turtle       #导入turtle模块
+import turtle       #导入turtle模块
+turtle.showturtle()    #显示箭头
+turtle.write("高淇")    #写字符串
+turtle.forward(300)   #前进300像素
+turtle.color("red")    #画笔颜色改为red
+turtle.left(90)      #箭头左转90度
+turtle.forward(300)
+turtle.goto(0,50)    #去坐标（0,50）
+turtle.goto(0,0)
+turtle.penup()     #抬笔。这样，路径就不会画出来
+turtle.goto(0,300)
+turtle.pendown()    #下笔。这样，路径就会画出来
+turtle.circle(100)    #画圆
+turtle.done()      #程序结束，保持窗口存在
+
+import turtle       #导入turtle模块
+turtle.showturtle()    #显示箭头
+turtle.write("高淇")    #写字符串
+turtle.forward(300)   #前进300像素
+turtle.color("red")    #画笔颜色改为red
+turtle.left(90)      #箭头左转90度
+turtle.forward(300)
+turtle.goto(0,50)    #去坐标（0,50）
+turtle.goto(0,0)
+turtle.penup()     #抬笔。这样，路径就不会画出来
+turtle.goto(0,300)
+turtle.pendown()    #下笔。这样，路径就会画出来
+turtle.circle(100)    #画圆
+turtle.done()      #程序结束，保持窗口存在
